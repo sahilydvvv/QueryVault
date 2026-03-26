1c332ebb3b0e705e5e8b8ace13dfcc30f058015d
sahil <sahhillyddvv@gmail.com>
2026-03-26 11:31:16 +0530

    added groq api and document loader basic setup code

---
+++ documentLoader/load_document.py
@@ -0,0 +1,5 @@
+from langchain_community.document_loaders import PyPDFLoader
+
+file_path = "C:\\Users\\sahil\\Desktop\\resume final.pdf"
+loader = PyPDFLoader(file_path)
+docs = loader.load()
---
+++ llm_invoke.py
@@ -0,0 +1,24 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from langchain_groq import ChatGroq
+from langchain_core.prompts import ChatPromptTemplate
+from documentLoader.load_document import docs
+
+template = ChatPromptTemplate.from_messages([
+    ("system", "You are a helpful assistant. summarize the following document in one line."),
+    ("user", "{input}")
+])
+
+prompt = template.invoke({
+    "input": f"Answer the question based on the following document: {docs[0].page_content}"
+})
+
+llm = ChatGroq(
+    model="llama-3.3-70b-versatile",
+    temperature=0,
+    max_tokens=None,
+)
+
+response = llm.invoke(prompt)
+print(response.content)
