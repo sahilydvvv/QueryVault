57835654e2dae46448812a39d6acc8ea7e74a03b
sahil <sahhillyddvv@gmail.com>
2026-03-30 09:03:14 +0530

    created backend and frontend folder and moved files into backend folder

--- backend/load_document.py
+++ backend/load_document.py
@@ -1,6 +1,6 @@
 from langchain_community.document_loaders import PyPDFLoader
 
-file_path = "./docs/sample.pdf"
+file_path = "C:\\Users\\sahil\\Desktop\\resume final.pdf"
 loader = PyPDFLoader(file_path)
 docs = loader.load()
 # print(docs[0].page_content)
